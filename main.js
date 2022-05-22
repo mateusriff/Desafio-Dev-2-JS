80248d2ed604c9ca93afe34d34e8ede6f674d28d
mateusriff <mateusnriff@gmail.com>
2022-05-22 17:02:19 -0300

    Add collision on main.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+//NOTE: My resolution is 1920x1080 and Pedro was "disappearing" before colliding with the actual border. 
+//To avoid that, I shrinked the borders by a few pixels, although that might stop him before reaching the 
+//borders on devices with different resolutions.
+
+//initializes variables
+
 const character = document.getElementsByClassName("character")[0];
 const containerCharacter = document.getElementsByClassName("container-character")[0];
 
-
+//defines Pedro's speed
 const VELOCITY = 10;
 
+//defines the width and height of the screen as variables. They're used later  
 const SCREEN_WIDTH = screen.width;
 const SCREEN_HEIGHT = screen.height;
+
+//defines Pedro's initial position 
 
 let xPosition = 500;
 let yPosition = 300;
@@ -13,7 +22,12 @@ let yPosition = 300;
 const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
 const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];
 
+//movement controls
+
 window.addEventListener("keydown", (event) => {
+    
+    //detects pressed keys
+    
     const key  = event.key;
 
     const keyPressedAvaiable =  keysAvaiable.some((currentKey) => {
@@ -26,27 +40,37 @@ window.addEventListener("keydown", (event) => {
         if(character.classList.contains(direction)) character.classList.remove(direction);
     })
 
+    //conditions movement to key being pressed
 
-    if(key === "ArrowUp"){
+    if(key === "ArrowUp") {
         character.classList.add("turnUp");
-        yPosition -= VELOCITY;
+        //conditions movement to proximity to screen border (collision)
+        if(yPosition > 0){
+            yPosition -= VELOCITY;  
+        }  
     }
 
     if(key === "ArrowDown"){
         character.classList.add("turnDown");
-        yPosition += VELOCITY;
+        if(yPosition < SCREEN_HEIGHT - 75) {
+            yPosition += VELOCITY;
+        }
     }
 
     if(key === "ArrowLeft"){
         character.classList.add("turnLeft");
-        xPosition -= VELOCITY;
+        if(xPosition > -25){
+            xPosition -= VELOCITY;  
+        } 
     }
 
     if(key === "ArrowRight"){
         character.classList.add("turnRight");
-        xPosition += VELOCITY;
+        if(xPosition < SCREEN_WIDTH - 75) {
+            xPosition += VELOCITY;
+        }
     }
 
     containerCharacter.style.top = `${yPosition}px`;
-    containerCharacter.style.left = `${xPosition}px`
+    containerCharacter.style.left = `${xPosition}px`;
 });
